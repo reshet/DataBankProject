a32999187b41eff477a2ec4d4d3664b5111e527e
Igor Reshetnev <reshet.ukr@gmail.com>
2012-03-14 11:19:54 +0200

    last days

---
+++ DatabankNewest/src/com/mresearch/databank/client/views/DBfillers/MetaUnitEntityItemRegistrator.java
@@ -0,0 +1,6 @@
+package com.mresearch.databank.client.views.DBfillers;
+
+public interface MetaUnitEntityItemRegistrator
+{
+  public void populateItemsLinksTo(Long paramLong, String paramString);
+}
