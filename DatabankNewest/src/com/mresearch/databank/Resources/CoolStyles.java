20371bbee7804cdbff1f19755047d3d5eef54483
Igor Reshetnev <reshet.ukr@gmail.com>
2012-03-16 11:23:21 +0200

    added prostyna view cssed 3

---
+++ DatabankNewest/src/com/mresearch/databank/Resources/CoolStyles.java
@@ -0,0 +1,9 @@
+package com.mresearch.databank.Resources;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface CoolStyles extends CssResource {
+
+	String category();
+
+}
---
+++ DatabankNewest/src/com/mresearch/databank/Resources/CoolStylesBundle.java
@@ -0,0 +1,10 @@
+package com.mresearch.databank.Resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+
+public interface CoolStylesBundle extends ClientBundle {
+
+	@Source("CoolStyles.css")
+	CoolStyles coolStyles();
+
+}
